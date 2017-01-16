dd9be1a5e8dc99795046a81fae3752754374b7fe
ar4enemist <ar4enemist@gmail.com>
2017-01-16 18:27:35 +0200

    header block

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/App';
 import './css/index.css';
+import './css/reset.css';
 
 ReactDOM.render(
   <App />,
--- src/js/Header.js
+++ src/js/Header.js
@@ -1,7 +1,7 @@
 /**
  * Created by Toha on 16.01.2017.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../css/Header.css';
 
 class Header extends Component {
@@ -9,8 +9,19 @@ class Header extends Component {
         return (
             <div className="Header">
                 <div className="Header__row Header__row--top"></div>
-                <div className="Header__row Header__row--bottom"></div>
+                <div className="Header__row Header__row--bottom">
+                    <ul className="Header__menu" >
+                    <li className="Header__menu_li">Home</li>
+                    <li className="Header__menu_li">About</li>
+                    <li className="Header__menu_li">Collections</li>
+                    <li className="Header__menu_li">Store location</li>
+                    <li className="Header__menu_li">Contacts</li>
+                    </ul>
+            <input type="text" name="search" className="Header__search" />
+
+                </div>
             </div>
+
         );
     }
 }
