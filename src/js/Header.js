c600fd8e9d071eb8e0cdbeed0dc9c82c4c832caf
Anton <antonakimov515@gmail.com>
2017-02-05 22:34:06 +0300

    top links

--- src/js/Header.js
+++ src/js/Header.js
@@ -8,30 +8,42 @@ class Header extends Component {
         return (
             <div className="Header">
                 <div className="Header__row Header__row--top">
-                    <div className="Header__top_right_block">
-                        <ul className="Header__top_links">
-                            <li className="Header__top_link">
-                                Login
-                            </li>
-                            <li className="Header__top_link">
-                                Help
-                            </li>
-                        </ul>
-                        <div className="Header__mini_cart"></div>
+                    <div className="Header__container">
+                        <div className="Header__top_right_block">
+                            <ul className="Header__top_links">
+                                <li className="Header__top_link">
+                                    Login
+                                </li>
+                                <li className="Header__top_link">
+                                    Help
+                                </li>
+                            </ul>
+                            <div className="Header__mini_cart">
+                                <div className="Header__mini_cart_icon"></div>
+                                <div className="Header__mini_cart_text">
+                                    My bag (0) items
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="Header__row Header__row--bottom">
-                    <ul className="Header__menu">
-                        <li className="Header__menu_li">Home</li>
-                        <li className="Header__menu_li">About</li>
-                        <li className="Header__menu_li">Collections</li>
-                        <li className="Header__menu_li">Store location</li>
-                        <li className="Header__menu_li">Contacts</li>
-                    </ul>
+                    <div className="Header__container">
+                        <ul className="Header__menu">
+                            <li className="Header__menu_li">Home</li>
+                            <li className="Header__menu_li">About</li>
+                            <li className="Header__menu_li">Collections</li>
+                            <li className="Header__menu_li">Store location</li>
+                            <li className="Header__menu_li">Contacts</li>
+                        </ul>
 
-                    <input type="text" name="search" className="Header__search" placeholder="Keyword or product code"/>
-                    <span className="Header__search_icon"></span>
+                        <div className="Header__search_wrapper">
+                            <input type="text" name="search" className="Header__search" placeholder="Keyword or product code"/>
+                            <span className="Header__search_icon"></span>
+                        </div>
 
+
+                    </div>
                 </div>
             </div>
 
