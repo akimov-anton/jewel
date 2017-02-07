1e5863a6525740f65b2b097e88c06435f3827e3d
Anton <antonakimov515@gmail.com>
2017-02-07 22:34:46 +0300

    Footer

---
+++ src/js/Footer.js
@@ -0,0 +1,81 @@
+/**
+ * Created by Toha on 07.02.2017.
+ */
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="Footer">
+                <div className="Footer__row">
+                    <div className="Footer__container">
+                        <div className="Footer__col">
+                            <div className="Footer__col_title">Services</div>
+                            <ul className="Footer__col_list">
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="Footer__col">
+                            <div className="Footer__col_title">Services</div>
+                            <ul className="Footer__col_list">
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="Footer__col">
+                            <div className="Footer__col_title">Services</div>
+                            <ul className="Footer__col_list">
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="Footer__col">
+                            <div className="Footer__col_title">Services</div>
+                            <ul className="Footer__col_list">
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                                <li className="Footer__col_option">
+                                    Text
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="Footer__row">
+                    <div className="Footer__copy">
+                        Copyright 2017
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
