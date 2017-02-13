3a051dda07d3530d86eba1d2bf0a3462ff569ae7
ar4enemist <ar4enemist@gmail.com>
2017-02-13 21:23:59 +0200

    slider_img

---
+++ src/js/TagLine.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class TagLine extends Component {
+    render() {
+        return (
+            <div className="TagLine">
+                <div className="TagLine__container">
+
+                    <span><img src="/imgs/TagLine/tagline.png"></img></span>
+
+
+
+
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default TagLine;
