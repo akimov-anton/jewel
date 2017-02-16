855751f646ce36920a1c5f3e4089adc3d20d163a
ar4enemist <ar4enemist@gmail.com>
2017-02-16 22:52:51 +0200

    slider_img

--- src/js/components/Item.js
+++ src/js/components/Item.js
@@ -6,9 +6,23 @@ import React, { Component } from 'react';
 class Item extends Component {
     render() {
         return (
-            <div>
-                Item component
+            <div className="item">
+                <div className="item__container">
+                    <span><img src="/imgs/item/frontpic.png"></img></span>
+                    <span className="item__tag"> Test <br/> Text</span>
+
+
+
+
+                </div>
+                <div className="item__img"></div>
             </div>
+
+
+
+
+
+
         );
     }
 }
