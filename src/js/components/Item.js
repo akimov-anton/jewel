42467747b4cc1ae167a0ca18848a73633628ac59
Toha <antonakimov515@gmail.com>
2017-02-18 21:26:55 +0300

    item component

--- src/js/components/Item.js
+++ src/js/components/Item.js
@@ -6,36 +6,27 @@ import React, {Component} from 'react';
 class Item extends Component {
     render() {
         return (
-            <div className="item">
-                <div className="item__container">
-                    <span><img src="/imgs/item/frontpic.png"></img></span>
-                    <span className="item__tag"> Test <br/> Text</span>
-
-                    <ul className="item__menu">
-                        <li className="item__menu_li"><a href="#">1</a></li>
-                        <li className="item__menu_li"><a href="#">2</a></li>
-                        <li className="item__menu_li"><a href="#">3</a></li>
-
-                    </ul>
-
-
+            <div className="Item">
+                <div className="Item__row Item__row--top">
+                    <div className="Item__container">
+                        <div className="Item__top_image"></div>
+                    </div>
                 </div>
-
-
-
-
-                <div className="item__img_menu">
-
-
+                <div className="Item__row Item__row--menu">
+                    <div className="Item__container">
+                        <ul className="Item__menu">
+                            <li className="Item__menu_li"><a href="#">1</a></li>
+                            <li className="Item__menu_li"><a href="#">2</a></li>
+                            <li className="Item__menu_li"><a href="#">3</a></li>
+                        </ul>
+                    </div>
                 </div>
-
+                <div className="Item__row Item__row--content">
+                    <div className="Item__container">
+                        <div className="Item__content"></div>
+                    </div>
+                </div>
             </div>
-
-
-
-
-
-
         );
     }
 }
