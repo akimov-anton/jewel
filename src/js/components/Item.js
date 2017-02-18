f9965ba1765a34b512501e737dae6d783b905ac2
ar4enemist <ar4enemist@gmail.com>
2017-02-18 19:38:59 +0200

    slider_img

--- src/js/components/Item.js
+++ src/js/components/Item.js
@@ -2,7 +2,7 @@
  * Created by Toha on 16.02.2017.
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 class Item extends Component {
     render() {
         return (
@@ -10,19 +10,24 @@ class Item extends Component {
                 <div className="item__container">
                     <span><img src="/imgs/item/frontpic.png"></img></span>
                     <span className="item__tag"> Test <br/> Text</span>
-                </div>
-                <div className="item__img_menu">
 
                     <ul className="item__menu">
                         <li className="item__menu_li"><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
+                        <li className="item__menu_li"><a href="#">2</a></li>
+                        <li className="item__menu_li"><a href="#">3</a></li>
 
                     </ul>
+
+
                 </div>
 
 
 
+
+                <div className="item__img_menu">
+
+
+                </div>
 
             </div>
 
