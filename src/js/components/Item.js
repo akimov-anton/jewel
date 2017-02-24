cbb335e01c06e6b1d6d70f435e3ed8cb6b14f238
Toha <antonakimov515@gmail.com>
2017-02-24 23:29:00 +0300

    detail page

--- src/js/components/Item.js
+++ src/js/components/Item.js
@@ -28,13 +28,52 @@ class Item extends Component {
                             123
                         </div>
 
-                        <div className="Item__content">12312334</div>
-
-
-
-
-
-
+                        <div className="Item__content">
+                            <div className="Item__title">FERN CLAMPER BRACELET</div>
+                            <div className="Item__short_description_block">
+                                <div className="Item__short_description_title">Description:</div>
+                                <div className="Item__short_description">
+                                    Mexican Silver Fern Clamper Bracelet. 7 1/4INCH
+                                </div>
+                            </div>
+                            <hr className="Item__splitter"/>
+                            <div className="Item__rating_block">
+                            </div>
+                            <hr className="Item__splitter"/>
+                            <div className="Item__order_block">
+                                <div className="Item__order_left_block">
+                                    <div className="Item__order_key">Product code</div>
+                                    <div className="Item__order_value">C425001A</div>
+                                    <div className="Item__order_key">Best price</div>
+                                    <div className="Item__order_value">$ 820.00</div>
+                                </div>
+                                <div className="Item__order_right_block">
+                                    <button className="Item__add_to_cart_btn">Add to cart</button>
+                                </div>
+                            </div>
+                            <hr className="Item__splitter"/>
+                            <div className="Item__more_description_block">
+                                <div className="Item__more_description_title">
+                                    More Description:
+                                </div>
+                                <div className="Item__description_block">
+                                    <div className="Item__description_block_name">
+                                        Style note
+                                    </div>
+                                    <div className="Item__description_text">
+                                        Lorem Ipsums simply
+                                    </div>
+                                </div>
+                                <div className="Item__description_block Item__description_block--minimized">
+                                    <div className="Item__description_block_name">
+                                        Style note
+                                    </div>
+                                    <div className="Item__description_text">
+                                        Lorem Ipsums simply
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
